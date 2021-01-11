2e3ce621aa1aee976aaf926c47082c50048077a2
Malachi Spencer <malachispencer@MA309.fritz.box>
2021-01-11 14:43:00 +0000

    Added test for #deposit to raise error if amount is not a number

--- bankAccount.test.js
+++ bankAccount.test.js
@@ -17,8 +17,14 @@ describe('BankAccount', () => {
 
     test('raises an error if date format is not valid', () => {
       expect(() => { 
-        bankAccount.deposit(1000, '100121');
+        bankAccount.deposit(1000, '2021/01/11');
       }).toThrow('Please enter valid date in the format YYYY-MM-DD');
+    });
+
+    test('raises an error if the amount is not a number', () => {
+      expect(() => {
+        bankAccount.deposit('wrong', '2021-01-11');
+      }).toThrow('Amount must be an a number');
     });
   });
 });
