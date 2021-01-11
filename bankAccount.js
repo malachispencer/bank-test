e7f33f157087ae419c76b52dd7a212a92938a678
Malachi Spencer <malachispencer@MA309.fritz.box>
2021-01-11 14:38:07 +0000

    #deposit throws error if date is invalid

--- bankAccount.js
+++ bankAccount.js
@@ -6,7 +6,23 @@ class BankAccount {
   }
 
   deposit(amount, date) {
-    
+    if (this.#isInvalidDate(date)) { 
+      throw 'Please enter valid date in the format YYYY-MM-DD';
+     }
+  }
+
+  #isInvalidDate(date) {
+    const validFormat = new RegExp(/^\d{4}-\d{2}-\d{2}$/);
+
+    if (!validFormat.test(date)) {
+      return true;
+    }
+
+    if (new Date(date) === 'Invalid Date') { 
+      return true; 
+    }
+
+    return false;
   }
 }
 
--- bankAccount.test.js
+++ bankAccount.test.js
@@ -10,9 +10,15 @@ describe('BankAccount', () => {
   describe('#deposit', () => {
     test('allows client to enter an amount and a date', () => {
       const depositSpy = jest.spyOn(bankAccount, 'deposit');
-      bankAccount.deposit(1000, '11/01/21');
+      bankAccount.deposit(1000, '2021-01-11');
 
-      expect(depositSpy).toHaveBeenCalledWith(1000, '11/01/21');
+      expect(depositSpy).toHaveBeenCalledWith(1000, '2021-01-11');
+    });
+
+    test('raises an error if date format is not valid', () => {
+      expect(() => { 
+        bankAccount.deposit(1000, '100121');
+      }).toThrow('Please enter valid date in the format YYYY-MM-DD');
     });
   });
 });
